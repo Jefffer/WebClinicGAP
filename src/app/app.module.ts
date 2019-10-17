f69fd95b5186638669f95431d60382d6edeb792e
Jefffer <jefre123@hotmail.com>
2019-10-17 09:43:46 -0500

    Bootstrap config. Patient component creation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PatientsComponent } from './patients/patients.component';
+import { PatientComponent } from './patients/patient/patient.component';
+import { PatientListComponent } from './patients/patient-list/patient-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientsComponent,
+    PatientComponent,
+    PatientListComponent
   ],
   imports: [
     BrowserModule
