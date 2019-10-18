f2dca1eeb1042e10923a584f71be0be713e398f1
Jefffer <jefre123@hotmail.com>
2019-10-18 09:09:58 -0500

    hidden input idAppointment added.

--- src/app/appointments/appointment-list/appointment-list.component.ts
+++ src/app/appointments/appointment-list/appointment-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppointmentService } from 'src/app/shared/appointment.service';
+import { Appointment } from 'src/app/shared/appointment.model';
 
 @Component({
   selector: 'app-appointment-list',
@@ -13,5 +14,18 @@ export class AppointmentListComponent implements OnInit {
   ngOnInit() {
     this.service.refresList();
   }
+
+  populateForm(pat : Appointment){
+    this.service.formData = Object.assign({},pat);
+  }
+
+  // onDelete(id : number){
+  //   if(confirm("Do you want to delete this Patient?")){
+  //     this.service.deletePatient(id).subscribe(res => {
+  //       this.toastr.info('Deleted Successfully', 'Patients Administration');
+  //       this.service.refreshList();
+  //     });
+  //   }
+  // }
 
 }
